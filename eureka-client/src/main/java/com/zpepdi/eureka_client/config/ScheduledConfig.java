1001d4073998a68a993e7f163f11c239750ce195
root <1105685461@qq.com>
2022-09-29 16:00:46 +0800

    2022-9-29

--- eureka-client/src/main/java/com/zpepdi/eureka_client/config/ScheduledConfig.java
+++ eureka-client/src/main/java/com/zpepdi/eureka_client/config/ScheduledConfig.java
@@ -52,21 +52,21 @@ public class ScheduledConfig implements SchedulingConfigurer {
     public ScientificSystemService scientificSystemService;
 
 
-//    @Scheduled(cron = "0 0 3 23 * ?")
-//    public void createScientificProduce(){
-//        scientificSystemService.createScientificProduce(DateUtils.getDateMonth());
-//    }
-//
-//    @Scheduled(cron = "0 0 0/2 * * ?")
-//    @Async("taskExecutor")
-//    public void dataTransmission(){
-//        service.dataTransmissionService();
-//    }
-//
-//    @Scheduled(cron = "0 0 0 * * ?")
-//    public void queryIncomeInformation(){
-//        service.queryIncomeInformation();
-//    }
+    @Scheduled(cron = "0 0 3 23 * ?")
+    public void createScientificProduce(){
+        scientificSystemService.createScientificProduce(DateUtils.getDateMonth());
+    }
+
+    @Scheduled(cron = "0 0 0/2 * * ?")
+    @Async("taskExecutor")
+    public void dataTransmission(){
+        service.dataTransmissionService();
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void queryIncomeInformation(){
+        service.queryIncomeInformation();
+    }
 
 
 
